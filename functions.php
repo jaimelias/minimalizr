ff72df72311659aff13609991736ceff99b69347
jaimelias <escribeme@jaimelias.com>
2018-09-23 00:10:20 +0000

    removing emonji JS

--- functions.php
+++ functions.php
@@ -572,6 +572,10 @@ remove_action ('wp_head', 'rsd_link');
 remove_action( 'wp_head', 'wlwmanifest_link');
 remove_action( 'wp_head', 'wp_shortlink_wp_head');
 remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 function facebook_messenger()
 {
