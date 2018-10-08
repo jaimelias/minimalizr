37dd5f7db64bdf7be92a52d97da023a032ac7a7c
jaimelias <escribeme@jaimelias.com>
2018-10-08 03:06:37 +0000

    T_string remove

--- inc/minimal_sitemap/sitemap.php
+++ inc/minimal_sitemap/sitemap.php
@@ -43,12 +43,7 @@ class minimal_sitemap{
 			{
 				ob_start();
 				
-				?>
-				<?xml version="1.0" encoding="UTF-8"?>
-				<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-					xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
-					xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
-				<?php
+				echo '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">';
 				
 				while ($sitemap_query->have_posts())
 				{
@@ -70,10 +65,7 @@ class minimal_sitemap{
 					<?php
 				}
 				
-				?>
-				</urlset>
-				<?php
-				
+				echo '</urlset>';
 				$content = ob_get_contents();
 				ob_end_clean();
 				$output = $content;
