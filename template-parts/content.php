a7774fa47bd2d4dee58536ca2d97567dedf6fefb
jaimelias <escribeme@jaimelias.com>
2019-02-05 19:31:07 +0000

    if not thumbanail  deleting first col

--- template-parts/content.php
+++ template-parts/content.php
@@ -6,25 +6,31 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-	
-		<div class="pure-g gutters">
-			<div class="pure-u-1-3">
-				<?php if ( has_post_thumbnail() ) : ?>
-					<p><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-					<?php the_post_thumbnail('medium', array('class' => 'img-responsive')); ?>
-					</a></p>
-				<?php endif; ?>
-				<div class="entry-meta text-muted small light bottom-10">
-					<?php minimalizr_posted_on(); ?>
-				</div><!-- .entry-meta -->					
-			</div>		
-			<div class="pure-u-2-3">		
-				<?php the_title( sprintf( '<h2 class="entry-title small light uppercase"><a class="small normal" href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-				<div class="entry-content normal small">
-					<?php the_excerpt();?>
-				</div><!-- .entry-content -->
+		<?php if ( has_post_thumbnail() ) : ?>
+			<div class="pure-g gutters">
+				<div class="pure-u-1-3">
+					
+						<p><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+						<?php the_post_thumbnail('medium', array('class' => 'img-responsive')); ?>
+						</a></p>
+					
+					<div class="entry-meta text-muted small light bottom-10">
+						<?php minimalizr_posted_on(); ?>
+					</div><!-- .entry-meta -->					
+				</div>		
+				<div class="pure-u-2-3">		
+					<?php the_title( sprintf( '<h2 class="entry-title small light uppercase"><a class="small normal" href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+					<div class="entry-content normal small">
+						<?php the_excerpt();?>
+					</div><!-- .entry-content -->
+				</div>
 			</div>
-		</div>
+		<?php else: ?>
+			<?php the_title( sprintf( '<h2 class="entry-title small light uppercase"><a class="small normal" href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+			<div class="entry-content normal small">
+				<?php the_excerpt();?>
+			</div><!-- .entry-content -->		
+		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 
