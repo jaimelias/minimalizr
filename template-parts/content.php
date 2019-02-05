c837600f4ec2dc2449a193f885ec8dd49dd24200
jaimelias <escribeme@jaimelias.com>
2019-02-05 19:11:30 +0000

    fix post archive title

--- template-parts/content.php
+++ template-parts/content.php
@@ -8,7 +8,7 @@
 	<header class="entry-header">
 	
 		<div class="pure-g gutters">
-			<div class="pure-u-1-2">
+			<div class="pure-u-1-3">
 				<?php if ( has_post_thumbnail() ) : ?>
 					<p><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 					<?php the_post_thumbnail('medium', array('class' => 'img-responsive')); ?>
@@ -18,9 +18,9 @@
 					<?php minimalizr_posted_on(); ?>
 				</div><!-- .entry-meta -->					
 			</div>		
-			<div class="pure-u-1-2">		
-				<?php the_title( sprintf( '<h2 class="entry-title"><a class="small normal" href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-				<div class="entry-content light">
+			<div class="pure-u-2-3">		
+				<?php the_title( sprintf( '<h2 class="entry-title small light uppercase"><a class="small normal" href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+				<div class="entry-content normal small">
 					<?php the_excerpt();?>
 				</div><!-- .entry-content -->
 			</div>
