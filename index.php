ae9acc27c749a91fa4bc3aaff5dee6af228ac3da
jaimelias <escribeme@jaimelias.com>
2020-03-19 16:30:39 +0000

    removed uppercase class from blog

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 				<?php if ( is_home() && ! is_front_page() )
 				{
 					?>
-						<header class="text-center uppercase block hero padding-20">
+						<header class="text-center block hero padding-20">
 							<h1 class="page-title"><?php single_post_title(); ?></h1>
 						</header>				
 					<?php
