ca829a73f1ec1ff76a03284e1ce6728236d0a682
Jaime Castillo <escribeme@jaimelias.com>
2025-04-21 17:05:42 -0500

    returned loader

--- submodules/dy-core/loader.php
+++ submodules/dy-core/loader.php
@@ -1,41 +1,45 @@
 <?php
 
-if ( !defined( 'WPINC' )) exit;
+if ( !defined( 'WPINC' ) ) exit;
 
-if(!class_exists('Dy_Core_Init'))
+$plugin_dir_path = plugin_dir_path( __FILE__ );
+
+if(!defined('DY_CORE_FUNCTIONS'))
 {
-    #[AllowDynamicProperties]
-    class Dy_Core_Init {
-
-        public function __construct()
-        {
-            $this->plugin_dir_path = plugin_dir_path( __FILE__ );
-            $this->load_dependencies();
-            $this->init();
-        }
-        public function load_dependencies()
-        {
-            require_once $this->plugin_dir_path . 'functions.php';
-            require_once $this->plugin_dir_path . 'mailer.php';
-            require_once $this->plugin_dir_path . 'admin.php';
-            require_once $this->plugin_dir_path . 'public.php';
-            require_once $this->plugin_dir_path . 'wp-json.php';
-            require_once $this->plugin_dir_path . 'integrations/providers/providers.php';
-            //require_once $this->plugin_dir_path . 'integrations/orders/orders.php';
-        }
-        public function init()
-        {
-            new Dy_Mailer();
-            new Dynamic_Core_Admin();
-            new Dynamic_Core_Public();
-            new Dynamic_Core_WP_JSON();
-            new Dynamic_Core_Providers();
-            //new Dynamic_Core_Orders();
-        }
-    }
-
-    new Dy_Core_Init();
+    require_once $plugin_dir_path . 'functions.php';
 }
 
+if(!class_exists('Dy_Mailer'))
+{
+    require_once $plugin_dir_path . 'mailer.php';
+    new Dy_Mailer();
+}
 
-?>
+if(!class_exists('Dynamic_Core_Admin'))
+{
+    require_once $plugin_dir_path . 'admin.php';
+    new Dynamic_Core_Admin();
+}
+
+if(!class_exists('Dynamic_Core_Public'))
+{
+    require_once $plugin_dir_path . 'public.php';
+    new Dynamic_Core_Public();
+}
+
+if(!class_exists('Dynamic_Core_WP_JSON'))
+{
+    require_once $plugin_dir_path . 'wp-json.php';
+    new Dynamic_Core_WP_JSON();
+}
+
+if(!class_exists('Dynamic_Core_Providers'))
+{
+    require_once $plugin_dir_path . 'integrations/providers/providers.php';
+    new Dynamic_Core_Providers();
+}
+if(!class_exists('Dynamic_Core_Orders'))
+{
+    //require_once $plugin_dir_path . 'integrations/orders/orders.php';
+    //new Dynamic_Core_Orders();
+}
