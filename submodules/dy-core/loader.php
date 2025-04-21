f6793f78b77a0687b168ec22c6be737d49e319c3
Jaime Castillo <escribeme@jaimelias.com>
2025-04-21 11:48:00 -0500

    changed dy-core loader for class

--- submodules/dy-core/loader.php
+++ submodules/dy-core/loader.php
@@ -1,36 +1,41 @@
 <?php
 
-if ( !defined( 'WPINC' ) || class_exists('Dy_Core_Init')) exit;
+if ( !defined( 'WPINC' )) exit;
 
-#[AllowDynamicProperties]
-class Dy_Core_Init {
-    public function __construct()
-    {
-        $this->plugin_dir_path = plugin_dir_path( __FILE__ );
-        $this->load_dependencies();
-        $this->init();
+if(!class_exists('Dy_Core_Init'))
+{
+    #[AllowDynamicProperties]
+    class Dy_Core_Init {
+
+        public function __construct()
+        {
+            $this->plugin_dir_path = plugin_dir_path( __FILE__ );
+            $this->load_dependencies();
+            $this->init();
+        }
+        public function load_dependencies()
+        {
+            require_once $this->plugin_dir_path . 'functions.php';
+            require_once $this->plugin_dir_path . 'mailer.php';
+            require_once $this->plugin_dir_path . 'admin.php';
+            require_once $this->plugin_dir_path . 'public.php';
+            require_once $this->plugin_dir_path . 'wp-json.php';
+            require_once $this->plugin_dir_path . 'integrations/providers/providers.php';
+            //require_once $this->plugin_dir_path . 'integrations/orders/orders.php';
+        }
+        public function init()
+        {
+            new Dy_Mailer();
+            new Dynamic_Core_Admin();
+            new Dynamic_Core_Public();
+            new Dynamic_Core_WP_JSON();
+            new Dynamic_Core_Providers();
+            //new Dynamic_Core_Orders();
+        }
     }
-    public function load_dependencies()
-    {
-        require_once $this->plugin_dir_path . 'functions.php';
-        require_once $this->plugin_dir_path . 'mailer.php';
-        require_once $this->plugin_dir_path . 'admin.php';
-        require_once $this->plugin_dir_path . 'public.php';
-        require_once $this->plugin_dir_path . 'wp-json.php';
-        require_once $this->plugin_dir_path . 'integrations/providers/providers.php';
-        //require_once $this->plugin_dir_path . 'integrations/orders/orders.php';
-    }
-    public function init()
-    {
-        new Dy_Mailer();
-        new Dynamic_Core_Admin();
-        new Dynamic_Core_Public();
-        new Dynamic_Core_WP_JSON();
-        new Dynamic_Core_Providers();
-        //new Dynamic_Core_Orders();
-    }
+
+    new Dy_Core_Init();
 }
 
-new Dy_Core_Init();
 
 ?>
