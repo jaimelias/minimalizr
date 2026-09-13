2b2d7011029159a4825e672631b8c66210656036
Jaime Castillo <escribeme@jaimelias.com>
2026-09-13 16:34:32 -0500

    upgraded map.php with exclusions - 1.1.23

--- submodules/dy-core/loader.php
+++ submodules/dy-core/loader.php
@@ -4,7 +4,7 @@ if ( !defined( 'WPINC' )) exit;
 
 if(!class_exists('Dy_Core_Init'))
 {
-    define('DY_CORE_VERSION', '1.1.22');
+    define('DY_CORE_VERSION', '1.1.23');
 
     if ( !defined( 'DY_CORE_TEXTDOMAIN' ) ) {
         define( 'DY_CORE_TEXTDOMAIN', 'dycore' );
